2e7c2dccefaa0dd44bfe2eea0ab094d0b7eeb7c1
blastwind <andrewchen14250@gmail.com>
2022-09-01 21:28:24 -0400

    Initial Commit: XML Leaf Extraction Successful

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# This file is a required part of a package
+# It is placed among the test suites in order to fulfill python3 -m unittest requirements: 
+# That the tests must be importable from the top level directory of the project
---
+++ tests/test_XMLParsing.py
@@ -0,0 +1,59 @@
+import unittest
+
+from main import extractXMLLeaves
+
+
+class TestXMLParsing(unittest.TestCase):
+
+    def test_find_child_uncollapsed_simple(self):
+        xml = "<a></a>"
+        self.assertEqual(extractXMLLeaves(xml), ["<a></a>"])
+
+    def test_find_child_uncollapsed_nested(self):
+        xml = "<b><a></a></b>"
+        self.assertEqual(extractXMLLeaves(xml), ["<a></a>"])
+
+    def test_find_multiple_child_uncollapsed_nested(self):
+        xml = "<a><b>asdf</b><c>asd</c></a>"
+        self.assertEqual(extractXMLLeaves(xml), ["<b>asdf</b>", "<c>asd</c>"])
+
+    def test_find_multiple_child_uncollapsed_nested_two(self):
+        xml = "<a><b>asdf<c id='cid'>a</c></b><c>asd</c></a>"
+        self.assertEqual(extractXMLLeaves(xml), [
+                         "<c id='cid'>a</c>", '<c>asd</c>'])
+
+    def test_find_multiple_child_uncollapsed_nested_three(self):
+        xml = "<a><b>asdf<c id='cid'><node>node</node></c></b><c>asd<node></node></c></a>"
+        self.assertEqual(extractXMLLeaves(xml), [
+                         "<node>node</node>", '<node></node>'])
+
+    def test_find_child_collapsed_simple(self):
+        xml = "<a/>"
+        self.assertEqual(extractXMLLeaves(xml), ["<a/>"])
+
+    def test_find_child_collapsed_nested(self):
+        xml = "<b><a/></b>"
+        self.assertEqual(extractXMLLeaves(xml), ["<a/>"])
+
+    def test_find_multiple_child_collapsed(self):
+        xml = "<a id='ok'/> <b attr1='asdf' attr2='bzxc'  /> <c/>"
+        self.assertEqual(extractXMLLeaves(xml), [
+                         "<a id='ok'/>", "<b attr1='asdf' attr2='bzxc'  />", "<c/>"])
+
+    def test_find_multiple_child_collapsed_nested(self):
+        xml = "<c><a id='ok'/></c><d><e><b attr1='asdf' attr2='bzxc'  /></e></d><c/>"
+        self.assertEqual(extractXMLLeaves(xml), [
+                         "<a id='ok'/>", "<b attr1='asdf' attr2='bzxc'  />", "<c/>"])
+
+    def test_find_multiple_child_mixed(self):
+        xml = "<a></a><b/><c/>"
+        self.assertEqual(extractXMLLeaves(xml), ["<a></a>", "<b/>", "<c/>"])
+
+    def test_find_multiple_child_mixed_nested(self):
+        xml = "<e><a></a><b/><c/></e>"
+        self.assertEqual(extractXMLLeaves(xml), ["<a></a>", "<b/>", "<c/>"])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
